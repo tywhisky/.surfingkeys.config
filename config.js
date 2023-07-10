9b0c343453c6cf1f534992e00e53d274af316862
tywhisky <fty221@gmail.com>
2023-07-10 23:29:57 +0800

    Init Repo

---
+++ config.js
@@ -0,0 +1,18 @@
+import { DefaultTheme } from './themes/default.js';
+import { RosePineTheme } from './themes/rose-pine.js';
+// Choose Tabs
+api.map('gt', 'T');
+
+// Back and forward in history
+api.map('H', 'S');
+api.map('L', 'D');
+// Switch Tab to left and right
+api.map('J', 'E');
+api.map('K', 'R');
+// Open a link in a new tab
+api.map('F', 'C');
+// ESC
+api.map("<ctrl-[>", "esc");
+
+// set theme
+settings.theme = RosePineTheme;
