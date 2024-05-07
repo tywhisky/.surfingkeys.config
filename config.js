b92ec12bbc2cba86da2bdacb3ef535a5549c7df8
tywhisky <fty221@gmail.com>
2024-05-07 12:24:53 +0800

    Add search shortkey for Xiaohongshu

--- config.js
+++ config.js
@@ -20,11 +20,22 @@ settings.theme = RosePineTheme;
 
 // Add Search for Bilibili
 api.removeSearchAlias('b');
-api.addSearchAlias('b', 'bilibili', 'https://search.bilibili.com/all?keyword=', 's', 'https://s.search.bilibili.com/main/suggest?func=suggest&suggest_type=accurate&sub_type=tag&main_ver=v1&highlight=&upuser_num=3&term=?', function(response) {
-    var res = JSON.parse(response.text).result.tag;
-    return res.map(function(r){
-        return r.value;
-    });
-   },
-   {favicon_url: 'https://www.bilibili.com/favicon.ico'}
+api.addSearchAlias('b', 'bilibili', 'https://search.bilibili.com/all?keyword=', 's', 'https://s.search.bilibili.com/main/suggest?func=suggest&suggest_type=accurate&sub_type=tag&main_ver=v1&highlight=&upuser_num=3&term=?', function (response) {
+  var res = JSON.parse(response.text).result.tag;
+  return res.map(function (r) {
+    return r.value;
+  });
+},
+  { favicon_url: 'https://www.bilibili.com/favicon.ico' }
+);
+
+// Add search for Xiaohongshu
+api.removeSearchAlias('s');
+api.addSearchAlias('s', 'Xiaohongshu', 'https://www.xiaohongshu.com/search_result?keyword=', 's', 'https://edith.xiaohongshu.com/api/sns/web/v1/search/recommend?keyword=', function (response) {
+  var res = JSON.parse(response.text).result.data.sug_items;
+  return res.map(function (r) {
+    return r.text;
+  });
+},
+  { favicon_url: 'https://www.xiaohongshu.com/favicon.ico' }
 );
